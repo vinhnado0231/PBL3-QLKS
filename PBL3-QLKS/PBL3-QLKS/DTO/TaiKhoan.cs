c8d2108d9c77ab4a4954e813373f7f1049b2d43a
vinhnado0231 <vinh.nd0231@gmail.com>
2022-06-16 14:36:20 +0700

    add

--- PBL3-QLKS/PBL3-QLKS/DTO/TaiKhoan.cs
+++ PBL3-QLKS/PBL3-QLKS/DTO/TaiKhoan.cs
@@ -10,5 +10,8 @@ namespace PBL3_QLKS.DTO
     {
         public string Username1 { get; set; }
         public string Password1 { get; set; }
+        public string Username3 { get; set; }
+        public string Password3 { get; set; }
+        public string Password4 { get; set; }
     }
 }
